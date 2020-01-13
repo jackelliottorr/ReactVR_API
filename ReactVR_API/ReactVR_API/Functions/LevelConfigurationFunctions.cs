e7e36ba818449c50ff1472d90cfd9c2ec745b473
Vandalaz <jackelliottorr@gmail.com>
2020-01-13 20:53:55 +0000

    Implemented some functions and repositories
    - LevelConfiguration
    - TargetZone
    - OrganisationInvite
    - OrganisationMembership (only repo so far)
    
    *All tested using Postman

---
+++ ReactVR_API/ReactVR_API/Functions/LevelConfigurationFunctions.cs
@@ -0,0 +1,99 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using ReactVR_API.Models;
+using ReactVR_API.Repositories;
+
+namespace ReactVR_API.Functions
+{
+    public static class LevelConfigurationFunctions
+    {
+        [FunctionName("CreateLevelConfiguration")]
+        public static async Task<IActionResult> CreateLevelConfiguration(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "levelConfiguration")] HttpRequest req, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function(CreateLevelConfiguration) processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var levelConfiguration = JsonConvert.DeserializeObject<LevelConfiguration>(requestBody);
+
+            try
+            {
+                var levelConfigurationRepo = new LevelConfigurationRepository();
+                var newId = levelConfigurationRepo.CreateLevelConfiguration(levelConfiguration);
+
+                return new OkObjectResult($"LevelConfiguration created with id {newId}.");
+            }
+            catch (Exception exception)
+            {
+                return new BadRequestObjectResult(exception.Message);
+            }
+        }
+
+        [FunctionName("GetLevelConfigurationById")]
+        public static async Task<IActionResult> GetLevelConfigurationById(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "levelConfiguration/{LevelConfigurationId}")] HttpRequest req, ILogger log, Guid levelConfigurationId)
+        {
+            log.LogInformation("C# HTTP trigger function(GetLevelConfigurationById) processed a request.");
+
+            try
+            {
+                var levelConfigurationRepo = new LevelConfigurationRepository();
+                var levelConfiguration = levelConfigurationRepo.GetLevelConfigurationById(levelConfigurationId);
+
+                return new OkObjectResult(levelConfiguration);
+            }
+            catch (Exception exception)
+            {
+                return new BadRequestObjectResult(exception.Message);
+            }
+        }
+
+        [FunctionName("UpdateLevelConfiguration")]
+        public static async Task<IActionResult> UpdateLevelConfiguration(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "levelConfiguration")] HttpRequest req, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function(UpdateLevelConfiguration) processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var levelConfiguration = JsonConvert.DeserializeObject<LevelConfiguration>(requestBody);
+
+            try
+            {
+                var levelConfigurationRepo = new LevelConfigurationRepository();
+                levelConfigurationRepo.UpdateLevelConfiguration(levelConfiguration);
+
+                return new OkObjectResult($"Updated {levelConfiguration.Name}.");
+            }
+            catch (Exception exception)
+            {
+                return new BadRequestObjectResult(exception.Message);
+            }
+        }
+
+        [FunctionName("DeleteLevelConfiguration")]
+        public static async Task<IActionResult> DeleteLevelConfiguration(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "levelConfiguration/{LevelConfigurationId}")] HttpRequest req, ILogger log, Guid levelConfigurationId)
+        {
+            log.LogInformation("C# HTTP trigger function(DeleteLevelConfiguration) processed a request.");
+
+            try
+            {
+                var levelConfigurationRepo = new LevelConfigurationRepository();
+                levelConfigurationRepo.DeleteLevelConfiguration(levelConfigurationId);
+
+                return new OkObjectResult($"Deleted {levelConfigurationId}");
+            }
+            catch (Exception exception)
+            {
+                return new BadRequestObjectResult(exception.Message);
+            }
+        }
+    }
+}
---
+++ ReactVR_API/ReactVR_API/Repositories/TargetZoneRepository.cs
@@ -0,0 +1,79 @@
+﻿using Dapper;
+using ReactVR_API.HelperClasses;
+using ReactVR_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ReactVR_API.Repositories
+{
+    class TargetZoneRepository : BaseRepository
+    {
+        public Guid CreateTargetZone(TargetZone targetZone)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var parameters = new
+                {
+                    targetZone.TargetZoneShape,
+                    targetZone.TargetZoneX,
+                    targetZone.TargetZoneY,
+                    targetZone.TargetZoneZ
+                };
+
+                var sql = SqlCrudHelper.GetInsertStatement(parameters, targetZone.GetType().Name, "TargetZoneId");
+
+                Guid newId = db.ExecuteScalar<Guid>(sql, parameters);
+
+                return newId;
+            }
+        }
+
+        public TargetZone GetTargetZoneById(Guid targetZoneId)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var parameters = new { targetZoneId };
+                var sql = "select * from targetZone where targetZoneid = @targetZoneId";
+
+                var targetZone = db.QuerySingle<TargetZone>(sql, parameters);
+
+                return targetZone;
+            }
+        }
+
+        public void UpdateTargetZone(TargetZone targetZone)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var parameters = new
+                {
+                    targetZone.TargetZoneShape,
+                    targetZone.TargetZoneX,
+                    targetZone.TargetZoneY,
+                    targetZone.TargetZoneZ
+                };
+
+                var sql = SqlCrudHelper.GetUpdateStatement(parameters, targetZone.GetType().Name);
+                sql += " WHERE TargetZoneId = @TargetZoneId";
+
+                db.Execute(sql, targetZone);
+            }
+        }
+
+        public void DeleteTargetZone(Guid targetZoneId)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var parameters = new
+                {
+                    TargetZoneId = targetZoneId
+                };
+                var sql = "update targetZone set [IsDeleted] = 1 WHERE [TargetZoneId] = @TargetZoneId";
+
+                db.Execute(sql, parameters);
+            }
+        }
+    }
+}
